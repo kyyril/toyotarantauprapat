22626e8a90989db3c33b7c3bb05b93efc672cce2
kyyril <khairilrahman55@gmail.com>
2025-01-03 15:59:48 +0700

    feat: navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/ThemeProvider";
+import { Navigation } from "@/components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -13,8 +15,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "toyotalabuhanbatu",
-  description: "astra toyota auto 2000 cabang labuhanbatu",
+  title: "toyotarantauprapat",
+  description: "astra toyota auto 2000 cabang labuhanbatu rantauprapat",
 };
 
 export default function RootLayout({
@@ -27,7 +29,15 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Navigation />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,76 @@
+"use client";
+import Link from "next/link";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "./ui/navigation-menu";
+import { usePathname } from "next/navigation";
+import MobileMenu from "./MobileMenu";
+import ThemeToggler from "./ThemeToggle";
+import Image from "next/image";
+import { PhoneCall } from "lucide-react";
+
+export const navigationItems = [
+  {
+    name: "Beranda",
+    href: "/",
+  },
+  {
+    name: "Tentang",
+    href: "/tentang",
+  },
+  {
+    name: "Produk",
+    href: "/produk",
+  },
+  {
+    name: "Layanan",
+    href: "/layanan",
+  },
+];
+
+export function Navigation() {
+  const pathname = usePathname();
+  return (
+    <nav className="sticky top-0 z-50 max-w-7xl mx-auto px-4 md:px-8 py-3 grid grid-cols-12 outline-none border-none backdrop-blur-sm bg-primary-foreground/0.5">
+      <div className="col-span-6 flex md:col-span-3">
+        <Link href={"/"}>
+          <div className="flex flex-col">
+            <Image alt="logo" width={110} height={5} src={"/images/logo.png"} />
+            <span className="absolute text-[12px] ml-8 mt-3.5 opacity-80">
+              Rantauprapat
+            </span>
+          </div>
+        </Link>
+      </div>
+      <div className="hidden sm:flex justify-center items-center col-span-6">
+        <NavigationMenu>
+          <NavigationMenuList>
+            {navigationItems.map((item, index) => (
+              <NavigationMenuItem key={index}>
+                <Link href={item.href} legacyBehavior passHref>
+                  <NavigationMenuLink
+                    active={pathname === item.href}
+                    className={navigationMenuTriggerStyle()}
+                  >
+                    {item.name}
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+            ))}
+          </NavigationMenuList>
+          <ThemeToggler />
+        </NavigationMenu>
+      </div>
+      <div className="flex items-center justify-end md:col-span-3 col-span-6">
+        <div className="sm:hidden">
+          <ThemeToggler />
+          <MobileMenu />
+        </div>
+      </div>
+    </nav>
+  );
+}
