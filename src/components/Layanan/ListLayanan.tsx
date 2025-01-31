b538c3c5b34e95d1f2f9b29693a3ecdc3a791b74
kyyril <khairilrahman55@gmail.com>
2025-01-31 23:42:15 +0700

    fix:key

--- src/components/Layanan/ListLayanan.tsx
+++ src/components/Layanan/ListLayanan.tsx
@@ -33,9 +33,9 @@ const ListLayanan: React.FC = () => {
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4">
-      {layananList.map((layanan, index) => (
-        <Link href={`/layanan/${layanan.id}`}>
-          <CardLayanan key={layanan.id} layanan={layanan} />
+      {layananList.map((layanan) => (
+        <Link key={layanan.id} href={`/layanan/${layanan.id}`}>
+          <CardLayanan layanan={layanan} />
         </Link>
       ))}
     </div>
