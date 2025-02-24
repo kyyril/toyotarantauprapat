0c08b1393d102d8b509cf67eb432ff7e1e4cec1a
kyyril <khairilrahman55@gmail.com>
2025-02-24 10:50:13 +0700

    fix: update WhatsApp contact number in contact dialog component

--- src/components/FormContact.tsx
+++ src/components/FormContact.tsx
@@ -105,7 +105,7 @@ export default function ContactDialog() {
       });
 
       const waMessage = `Halo, saya ${form.nama}.%0APesan: ${form.pesan}`;
-      const waUrl = `https://wa.me/6282361456715?text=${waMessage}`;
+      const waUrl = `https://wa.me/62811612535?text=${waMessage}`;
       window.open(waUrl, "_blank");
       setForm({ nama: "", nomorHp: "", kategori: "", pesan: "" });
       setWasInteracted(false);
